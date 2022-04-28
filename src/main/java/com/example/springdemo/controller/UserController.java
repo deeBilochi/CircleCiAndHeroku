e0a5bbca44e76853f0a6ed016f1bbc6ac407f3de
Dharmendra Singh <dharmenn@thoughtworks.com>
2022-04-28 18:04:21 +0530

    Add sample spring boot app

---
+++ src/main/java/com/example/springdemo/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.springdemo.controller;
+
+import com.example.springdemo.model.User;
+import com.example.springdemo.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> users() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return  users;
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestParam String name, @RequestParam int age) {
+        User user = new User();
+        user.setAge(age);
+        user.setName(name);
+        return userRepository.save(user);
+    }
+
+
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable long userId) {
+        return userRepository.findById(userId).get();
+    }
+}
+
---
+++ src/main/java/com/example/springdemo/model/User.java
@@ -0,0 +1,41 @@
+package com.example.springdemo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
